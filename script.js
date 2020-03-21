6a5e41afff8a71522f23e0fbbb7d5492281b2881
marijan111 <marijanbao@gmail.com>
2020-03-21 14:37:49 +0100

    'finish'

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const generateTarget = function() {
   return Math.floor(Math.random() * 10);
 };
 
-const compareGuesses = function() {
-  //compare guesses
+const compareGuesses = function(humanGuess, compGuess, secretNum) {
+  const humanDifference = Math.abs(secretNum - humanGuess);
+  const compDifference = Math.abs(secretNum - compGuess);
+  return humanDifference <= compDifference;
 };
+
+const updateScore = function(winner) {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => currentRoundNumber++;
